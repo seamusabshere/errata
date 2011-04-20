b9c334598b115d45a1e20567216e9eaeae5d21d7
Seamus Abshere <seamus@abshere.net>
2011-04-20 13:19:49 -0500

    no more debug

--- test/helper.rb
+++ test/helper.rb
@@ -7,9 +7,6 @@ end
 Bundler.setup
 require 'test/unit'
 require 'shoulda'
-unless RUBY_VERSION >= '1.9'
-  require 'ruby-debug'
-end
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'errata'
